680f8b587b21215cc237aee00c90b78764aabb12
bhavya-dang <bhavya.dang127@gmail.com>
2025-03-03 22:05:49 +0530

    chore: / redirects to /rersources now

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,15 @@
 import nextra from "nextra";
 
-const withNextra = nextra({
-  // ... Other Nextra config options
-});
+const withNextra = nextra({});
 
-// You can include other Next.js configuration options here, in addition to Nextra settings:
 export default withNextra({
-  // ... Other Next.js config options
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/resources",
+        permanent: true,
+      },
+    ];
+  },
 });
