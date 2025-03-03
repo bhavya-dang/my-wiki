9f1c231596f75454d17d6f08b115a99c3ba17635
bhavya-dang <bhavya.dang127@gmail.com>
2025-03-03 21:41:06 +0530

    chore: fixed readme

---
+++ app/_meta.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  resources: "Resources",
+  tools: "Tools",
+  "###": {
+    type: "separator",
+  },
+  blogs: "",
+};
---
+++ app/blogs/_meta.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  apple: "Apple",
+  banana: "Banana",
+};
